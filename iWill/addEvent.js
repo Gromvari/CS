f3ef19cdeab41b691403637c67324c8ca0a48178
Gergo Romvari <gergo.romvari@gmail.com>
2018-02-19 14:07:28 -0500

    Storing Works
    Store now works, can add events to be displayed later

--- iWill/addEvent.js
+++ iWill/addEvent.js
@@ -20,6 +20,64 @@ function addEvent(){
 		Time: eTime
 	};
 	eventJSON = JSON.stringify(eventObj);
-	localStorage.setItem("test", eventJSON);
+	if(localStorage.getItem("index"))
+	{
+		var i = Number(localStorage.getItem("index"));
+		localStorage.setItem(i, eventJSON);
 		alert("Things were submitted" + eName + eDesc + eTime);
+	}
+	else
+	{
+		localStorage.setItem("index","0");
+		localStorage.setItem("0", eventJSON);
+		alert("Things were submitted" + eName + eDesc + eTime);
+	}
+}
+
+function addEventParam(eName, eDesc, eTime)
+{
+	var eventObj = {
+		Name: eName,
+		Desc: eDesc,
+		Time: eTime
+	};
+	eventJSON = JSON.stringify(eventObj);
+	
+	if(localStorage.getItem("index"))
+	{
+		var i = Number(localStorage.getItem("index"));
+		localStorage.setItem(i, eventJSON);
+		localStorage.setItem("index", i + 1);
+		alert("Things were submitted" + eName + eDesc + eTime);
+	}
+	else
+	{
+		localStorage.setItem("index","0");
+		localStorage.setItem("0", eventJSON);
+		localStorage.setItem("index", i + 1);
+		alert("Things were submitted" + eName + eDesc + eTime);
+	}
+	
+	// localStorage.setItem("test", eventJSON);
+		// alert("Things were submitted" + eName + eDesc + eTime);
+}
+
+function addEventFromHTML()
+{
+	var eName = document.forms["eventForm"]["eventName"].value;
+	var eDesc = document.forms["eventForm"]["eventDesc"].value;
+	var eTime = document.forms["eventForm"]["eventTime"].value;
+	if(eName == "") {
+		alert("Name must be filled out");
+		return false; 
+	}
+	else if(eDesc == ""){
+		alert("Desc must be filled out");
+		return false;
+	}
+	else if(eTime == ""){
+		alert("Time must be filled out");
+		return false;
+	}
+	addEventParam(eName, eDesc, eTime);
 }
--- iWill/viewEvent.js
+++ iWill/viewEvent.js
@@ -1,8 +1,21 @@
 function loadEvents()
 {
-	t = localStorage.getItem("test");
-	obj = JSON.parse(t);
-	document.getElementById("eventTableBody").innerHTML = 	"<tr><td>" + obj.Name + "</td>" +
+					
+	var index = Number(localStorage.getItem("index"));
+	for(i = 0; i < index; i++)
+	{
+		t = localStorage.getItem(i);
+		obj = JSON.parse(t);
+		document.getElementById("eventTableBody").innerHTML += 	
+															"<tr><td>" + obj.Name + "</td>" +
 															"<td>" + obj.Desc + "</td>" +
 															"<td>" + obj.Time + "</td></tr>";
+	}
+	
+	document.getElementById("testField").innerHTML = "Index value: " + localStorage.getItem("index");
+}
+
+function getEvent(i)
+{
+	
 }
