a7e72ac08cf163ddc8156d244e80877672df7ea0
Gergo Romvari <gergo.romvari@gmail.com>
2018-03-05 19:20:31 -0500

    JST catchup
    Demo for alert, add, view.
    Dete causeing issues at the moment

--- iWill/temp.js
+++ iWill/temp.js
@@ -4,13 +4,14 @@ var standardEvent = {
 	Time: 			new Date(),
 	AlertType:		'Standard'
 }
+var sound;
 
 function eventAlert(eventObj)
 {
 	var alertObj = eventObj.AlertType;
 	var alertSound = alertObj.Sound;
 }
-var sound;
+
 function eventAlertStandard(eventObj)
 {
 	sound = new Audio("alertPing.mp3");
@@ -40,4 +41,57 @@ function clearEventAlert( alert )
 	aPop.setAttribute('class', 'hideThisShit');
 	sound.pause();
 	clearTimeout(alert.t);
+}
+
+// CONTINUATION OF EVENTMANAGER
+function addEventFromHTML()
+{
+	var eName = document.forms["eventForm"]["eventName"].value;
+	var eDesc = document.forms["eventForm"]["eventDesc"].value;
+	var eTime = document.forms["eventForm"]["eventTime"].value;
+	var eWhen = document.forms["eventForm"]["eventWhen"].value;
+	if(eName == "" || eDesc == "" || eTime == "" || eWhen == "")
+	{
+		alert ("Something was left empty. JS Testing Environment has submitted a Standard Event");
+		addEventParam("Standard Event", "standard stuff", "?time?", "?time?");
+	}
+	else
+	{
+		addEventParam(eName, eDesc, eTime, eWhen);
+	}
+}
+
+function addEventParam(eName, eDesc, eTime, eWhen)
+{
+	var eventObj = {
+		Name: eName,
+		Desc: eDesc,
+		Time: eTime,
+		When: eWhen
+	};
+	eventJSON = JSON.stringify(eventObj);
+	alert(eventJSON);		//TESTING
+	if(sessionStorage.getItem("index") != null)
+	{
+		var i = Number(sessionStorage.getItem("index"));
+		sessionStorage.setItem(i, eventJSON);
+		sessionStorage.setItem("index", i + 1);
+		
+	}
+	else
+	{
+		sessionStorage.setItem("0", eventJSON);
+		sessionStorage.setItem("index", "1");
+	}
+}
+
+function clearAllEvents()
+{
+	sessionStorage.clear();
+	document.getElementById("eventTableBody").innerHTML = "";
+}
+
+function deleteEventIndex(eventID)
+{
+	sessionStorage.removeItem(eventID);
 }
---
+++ iWill/viewManager.js
@@ -0,0 +1,89 @@
+function loadAddEventDemo()
+{
+	var body = document.getElementById('body');
+	body.innerHTML = 
+	"<h1> Add Event </h1>"	+
+	"<form name='eventForm' onsubmit='return addEventFromHTML()' method='post'>"	+
+	"<table>"	+
+		"<tr><td>Name: </td>			<td><input type='text' name='eventName' class='addField'></td> </tr>"	+
+		"<tr><td>Description: </td>	<td><textarea type='text' name='eventDesc' class='addField'></textarea></td></tr> "	+
+		"<tr><td>Time: </td>			<td><input type='date' name='eventTime' class='addField'></td> </tr>"	+
+		"<tr><td>When?: </td>		<td><input type='text' name='eventWhen' class='addField'></td> </tr>"	+
+	"</table>"	+	
+	"<input type='submit' value'Add Event'>"	+
+	"</form>";	
+	
+	body.innerHTML += "<p id='eventText'></p><div id='alertPopup'></div>";		//alert functionality
+}
+
+function loadViewEventDemo()
+{
+	var body = document.getElementById('body');
+	body.innerHTML = 
+	"<h1> View Events </h1>" +
+	"<table id='eventTable'>"+
+		"<thead>"+
+		"<th>XXX</th>"+
+		"<th>Event Name</th>"+
+		"<th>Description</th>"+
+		"<th>Date</th>"+
+		"<th>When</th>"+
+		"</thead>"+
+		"<tbody id='eventTableBody'>"+
+		
+		"</tbody>"+
+	
+	"</table>";
+	
+	loadEvents();
+	body.innerHTML += '<input type="button" value="Doom Button" onclick="clearAllEvents()">';
+	body.innerHTML += "<p id='eventText'></p><div id='alertPopup'></div>";		//alert functionality
+	
+}
+
+function loadDeleteEventDemo()
+{
+	var body = document.getElementById('body');
+	body.innerHTML = 
+	"<h1> View Events </h1>" +
+	"<table id='eventTable'>"+
+		"<thead>"+
+		"<th>Delete</th>"+
+		"<th>Event Name</th>"+
+		"<th>Description</th>"+
+		"<th>Date</th>"+
+		"<th>When</th>"+
+		"</thead>"+
+		"<tbody id='eventTableBody'>"+
+		
+		"</tbody>"+
+	
+	"</table>";
+	
+	loadEvents();
+	body.innerHTML += '<input type="button" value="Doom Button" onclick="clearAllEvents()">';
+	body.innerHTML += "<p id='eventText'></p><div id='alertPopup'></div>";		//alert functionality
+}
+
+function loadEvents()
+	{
+		var index = Number(sessionStorage.getItem("index"));
+		for(i = 0; i < index; i++)
+		{
+			t = sessionStorage.getItem(i);
+			obj = JSON.parse(t);
+			createRow(obj, i);
+		}
+	}
+
+	function createRow(eventObj, i)
+	{
+		document.getElementById("eventTableBody").innerHTML +=
+			"<tr>"+
+				"<td>" +  "<button value=" +i+ " class='editEventButton' onclick='deleteEventIndex(" +i+ ")'>" + "</td>" +
+				"<td>" + eventObj.Name  + "</td>" +
+				"<td>" + eventObj.Desc  + "</td>" +
+				"<td>" + eventObj.Time  + "</td>" +
+				"<td>" + eventObj.When  + "</td>" +
+			"</tr>";
+	}
