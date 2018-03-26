7705e07f62a3158cb68be8bf9b9a4f0acb40bb61
Gromvari <33436162+Gromvari@users.noreply.github.com>
2018-03-26 11:20:42 -0400

    CoffeShop Work

--- iWill/_Scripts/alertManager.js
+++ iWill/_Scripts/alertManager.js
@@ -1,10 +1,14 @@
+//{		GLOBAL VARIABLES
+var alertStatus = false;
+
+var alertSound = new Audio("_Sounds/alertPing.mp3");
 var StandardEvent = {
 	e_name: "Standard Event Name",
 	e_desc: "just a quick one",
 	e_date: new Date(),
 	e_stat: "S_ACTIVE"
 };
-
+//}
 function startDrivers()
 {
 	console.log("Event Driver and Alert Driver Started");
@@ -17,75 +21,66 @@ function startDrivers()
 	//var t = setTimeout( dr, 2000);
 }
 
-function eventAlert(eventObj)
-{
-	  console.log("eventAlert()");							//play alert sound
-	sound = new Audio("_Sounds/alertPing.mp3");
-	sound.loop = true;
-	sound.play();
-	
-	var id = eventObj.e_id;
-	
-	var aPop = document.getElementById("alertPopup");			//create popup
-	aPop.innerHTML = 	'<b>' + eventObj.e_name 			+ '</b><br>' +
-								eventObj.e_desc 			+ '<br>' +
-								getDateString(new Date(eventObj.e_date) )		+ '<br>';
-	aPop.innerHTML += "<button onclick='clearEventAlert(" + id + ")'> Finished </button>";
-	aPop.setAttribute('class', 'alertPopupON');
-	
-						
-	var t = setTimeout( eventAlertExit , 10000 );					//clear alert after timeout	
-	function eventAlertExit(){
-		  console.log("eventAlertExit() : timout");
-		sound.pause();
-		aPop.innerHTML = "";
-		aPop.setAttribute('class', 'hideThisShit');
-	}
-}
-
-function clearEventAlert( id ) // stop the alert
-{
-	var aPop = document.getElementById("alertPopup");		//stop sound and remove alert popup
-	sound.pause();
-	aPop.innerHTML = "";
-	aPop.setAttribute('class', 'hideThisShit');
-	
-	var a = JSON.parse(sessionStorage.getItem("ALERT"));
-	var eventObj = a.pop();
-	  console.log( eventObj );
-	eventObj.e_stat = "PROCESSED";
-}
 
 function eventDriver()
 {
+	eDrive = setTimeout(eventDriver, 2000);		// loop eventDriver
 	var s = retriveSTORE();
-	var date = new Date();
-	for(i = 0; i < s.length ; i++)
+	for(i = 0; i < s.length ; i++)		// loop event list 
 	{
-		if( s[i].e_stat == "S_ACTIVE")
+		if( s[i].e_stat == "S_ACTIVE")	// process only active
 		{
 			var currDate = new Date();
-			var date = new Date(s[i].e_date);
-			  //console.log( date);
-			  //console.log( currDate);
-			if(	currDate.getFullYear() 	== date.getFullYear() && 
-				currDate.getMonth() 	== date.getMonth() &&
-				currDate.getDay()		== date.getDay() &&
-				currDate.getHours()		== date.getHours() &&
-				currDate.getMinutes()	== date.getMinutes() )
-			{
+			var date = new Date(s[i].e_date);			//find matching time 
+			if(isMatchingTime(currDate, date)
+			{	
+				// begin processing event, break loop 
 				  console.log("Matching time in Active found");
-				pushAlert( i );
+				s[i].e_stat = "S_PROCESSING"; 
+				updateSTORE( s ); 
+				clearTimeout(eDrive);
+				processEvent( i );
+				break;
 			}
 		}
 	}
 	
-	setTimeout(eventDriver, 2000);
+}
+
+
+
+// create alert, wait/handle alert, restart event driver 
+function processEvent( i )
+{
+	  console.log("processEvent(" + i + ")");
+	var s = retriveSTORE();
+	var eventObj = s[i];				//assuming S_PROCESSING
+	s[i].e_stat = "S_ALERT";
+	updateSTORE(s);
+	
+	createAlert(eventObj);				//create alert 
+	
+	setTimeout(deleteAlert, 10000);		// after 10 sec, clear alert
+	setTimeout(eventDriver, 10000);		// after 10 sec, star eventDriver back up 
+}
+
+function createAlert(eventObj)
+{	  console.log("createAlert()");
+	var date = new Date(eventObj.e_date);
+	
+	playAlertSound();
+	createAlertPopup(eventObj.e_name, eventObj.e_desc, date); //might have to cast as date 
+}
+
+function deleteAlert()
+{	  console.log("deleteAlert()");
+	stopAlertSound();
+	deleteAlertPopup();
 }
 
 function alertDriver()
 {
-	if(sessionStorage.getItem("ALERT") == null)
+	if(sessionStorage.getItem("ALERT") == null)		//init "ALERT"
 	{
 		var ap = new Array();
 		sessionStorage.setItem("ALERT", JSON.stringify(ap));
@@ -100,6 +95,7 @@ function alertDriver()
 	
 	
 	sessionStorage.setItem("ALERT", JSON.stringify(a));
+	
 	//for now just loop
 	var t = setTimeout(alertDriver, 2000);
 	
@@ -125,7 +121,7 @@ function pushAlert(i) // using storage index
 }
 
 
-//{ 	SHORTCUT TO STORE/LOAD
+//{ 	Helping functions
 function retriveSTORE()
 {
 	return JSON.parse(sessionStorage.getItem("STORE"));
@@ -134,6 +130,36 @@ function retriveSTORE()
 function updateSTORE( s )
 {
 	sessionStorage.setItem("STORE", JSON.stringify(s));
+}
+
+function playAlertSound()
+{	  console.log("playAlertSound()");
+	alertSound.loop = true;
+	alertSound.play();
+}
+function stopAlertSound()
+{	  console.log("stopAlertSound()");
+	alertSound.pause();
+}
+
+function createAlertPopup(name, desc, date)
+{	  console.log("createAlertPopup()");
+	var aPop = document.getElementById("alertPopup");
+	aPop.innerHTML = 	'<b>' + name+ '</b><br>' +
+								desc+ '<br>' +
+								getDateString(date)	+ '<br>';
+	aPop.setAttribute('class', 'alertPopupON');	//make visible 						
+	alertStatus = true;
+							
+	//aPop.innerHTML += "<button onclick='clearEventAlert(" + id + ")'> Finished </button>";
+	//aPop.setAttribute('class', 'alertPopupON');
+}
+function deleteAlertPopup()
+{	  console.log("deleteAlertPopup()");
+	var aPop = document.getElementById("alertPopup");
+	aPop.innerHTML = "";
+	aPop.setAttribute('class', 'hidden');
+	alertStatus = false;
 }
 function getDateString( date )
 {	
@@ -146,5 +172,17 @@ function getDateString( date )
 	else 
 		r += date.getHours() + ":" + min + " AM";
 	return r;
+}
+
+// compare 2 Date objects up till minutes
+function isMatchingTime(currDate, date )
+{
+	if(	currDate.getFullYear() 	== date.getFullYear() && 	
+				currDate.getMonth() 	== date.getMonth() &&
+				currDate.getDay()		== date.getDay() &&
+				currDate.getHours()		== date.getHours() &&
+				currDate.getMinutes()	== date.getMinutes() )
+		return true;
+	return false;
 }
 //}
